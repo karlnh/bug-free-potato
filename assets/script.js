af3a27a0dfa50c9f06dd225bb0ee4e81b2cfdf07
karlnh <hubigkarl@gmail.com>
2022-11-22 21:55:36 -0600

    add current time display

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,7 @@
+// DOM elements
+var saveBtnEl = $('.saveBtn');
+var currentTimeEl = $('#currentDay');
+
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
@@ -8,16 +12,27 @@ $(function () {
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
+
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  function displayCurrentTime() {
+    var timerInterval = setInterval(function () {
+      var rightNow = dayjs().format('[It is currently] hh:mm:ss a [on] MMM DD, YYYY[.]');
+      currentTimeEl.text(rightNow);
+    }, 1000);
+  }
+
+  // running functions
+  displayCurrentTime();
 });
