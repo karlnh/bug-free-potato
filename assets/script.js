14f95005995c8a04c854d492335214d6badbd29a
karlnh <hubigkarl@gmail.com>
2022-11-23 00:42:53 -0600

    comment js

--- assets/script.js
+++ assets/script.js
@@ -9,29 +9,29 @@ var hourNow = dayjs().hour(23);
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  function displayHourElements() {
+  // on page load, generates hour divs and colors them.
+  function renderHourElements() {
     let countHour = 9;
     let realHour = 0;
     let meridiem = "AM";
-    for (i = 0; i < 9; i++) { // 9 hours in this workday
+    for (i = 0; i < 9; i++) { // 9 hours in the workday
       var hourBlockDiv = $('<div>');
+      hourBlockDiv.addClass("row time-block");
       hourBlockDiv.attr('id', "hour-"+countHour);
-      // checking AM or PM for each element created
-      if (countHour < 12) {
+      // checking AM or PM for each element created:
+      if (countHour < 12) { // before noon
         realHour = countHour;
         countHour++;
-      } else if (countHour === 12) {
+      } else if (countHour === 12) { // noon
         meridiem = "PM";
         realHour = 12;
         countHour++;
-      } else {
+      } else { // afternoon
         meridiem = "PM";
         realHour = countHour - 12;
         countHour++;
       };
-      hourBlockDiv.addClass("row time-block")
-
-      // changing color of div through classes
+      // changing color of div through classes:
       if (countHour-1 !== hourNow.$H) {
         if (countHour-1 < hourNow.$H) {
           hourBlockDiv.addClass("past");
@@ -41,12 +41,12 @@ $(function () {
       } else {
         hourBlockDiv.addClass("present");
       }
-      // if (the counthour is not equal to currenthour)
-      // adding example stylization for each element
-      hourBlockDiv.html("<div class='col-2 col-md-1 hour text-center py-3'>"+realHour+meridiem+"</div>"+
-      "<textarea class='col-8 col-md-10 description' rows='3'> </textarea>"+
+      // adding example stylization for each element:
+      hourBlockDiv.html(
+      "<div class='col-2 col-md-1 hour text-center py-3'>" + realHour+meridiem + "</div>" +
+      "<textarea class='col-8 col-md-10 description' rows='3'> </textarea>" +
       "<button class='btn saveBtn col-2 col-md-1' aria-label='save'><i class='fas fa-save' aria-hidden='true'></i></button>"
-      );
+      ); // button for each block can be selected by hourBlockDiv.querySelector("hour-(num)").
       hourContainerEl.append(hourBlockDiv);
     };
 
@@ -60,27 +60,19 @@ $(function () {
   // useful when saving the description in local storage?
 
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-
-
-  //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // displays the current date/time in page header.
   function displayCurrentTime() {
     var timerInterval = setInterval(function () {
       var rightNow = dayjs().format('[It is currently] hh:mm:ss a [on] MMM DD, YYYY[.]');
       currentTimeEl.text(rightNow);
-    }, 1000);
+    }, 1000); // updates every second
   }
 
   // running functions
-  displayHourElements();
+  renderHourElements();
   displayCurrentTime();
 });
