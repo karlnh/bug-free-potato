5208784c83f8b52e159a7168d03dc8a56d0ba2b6
karlnh <hubigkarl@gmail.com>
2022-11-22 23:40:57 -0600

    add function for rendering hour elements

--- assets/script.js
+++ assets/script.js
@@ -1,11 +1,48 @@
 // DOM elements
 var saveBtnEl = $('.saveBtn');
 var currentTimeEl = $('#currentDay');
+var hourContainerEl = $('#hour-container')
+
+var hourNow = dayjs().hour(23);
+console.log(dayjs().hour(23));
 
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
+  function displayHourElements() {
+    let countHour = 9;
+    let realHour = 0;
+    let meridiem = "AM";
+    for (i = 0; i < 9; i++) { // 9 hours in this workday
+      var hourBlockDiv = $('<div>');
+      hourBlockDiv.attr('id', "hour-"+countHour);
+      // checking AM or PM for each element created
+      if (countHour < 12) {
+        realHour = countHour;
+        countHour++;
+      } else if (countHour === 12) {
+        meridiem = "PM";
+        realHour = 12;
+        countHour++;
+      } else {
+        meridiem = "PM";
+        realHour = countHour - 12;
+        countHour++;
+      };
+      hourBlockDiv.addClass("row time-block past")
+      console.log("24-hour time: ", countHour-1); //logs time in 24 hour format
+      // if (the counthour is not equal to currenthour)
+      // adding example stylization for each element
+      hourBlockDiv.html("<div class='col-2 col-md-1 hour text-center py-3'>"+realHour+meridiem+"</div>"+
+      "<textarea class='col-8 col-md-10 description' rows='3'> </textarea>"+
+      "<button class='btn saveBtn col-2 col-md-1' aria-label='save'><i class='fas fa-save' aria-hidden='true'></i></button>"
+      );
+      hourContainerEl.append(hourBlockDiv);
+    };
+
+  }
+
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
@@ -19,6 +56,8 @@ $(function () {
   // attribute of each time-block be used to conditionally add or remove the
   // past, present, and future classes? How can Day.js be used to get the
   // current hour in 24-hour time?
+  console.log("I am selecting the text of the first hour element:", document.querySelector("#hour-9 > div").childNodes[0]);
+
 
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
@@ -34,5 +73,6 @@ $(function () {
   }
 
   // running functions
+  displayHourElements();
   displayCurrentTime();
 });
